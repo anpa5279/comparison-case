8647ca34f1a5d59dae35b55405581498c5685934
anpa5279 <anpa5279@colorado.edu>
2025-04-25 07:59:07 -0600

    updating SGS name

--- comparison.jl
+++ comparison.jl
@@ -99,7 +99,7 @@ model = NonhydrostaticModel(; grid, buoyancy, #coriolis,
                             advection = WENO(),
                             timestepper = :RungeKutta3,
                             tracers = (:T),
-                            closure = SmagorinskyLilly(smagorinsky = 0.1),
+                            closure = Smagorinsky(smagorinsky = 0.1),
                             stokes_drift = UniformStokesDrift(∂z_uˢ=new_dUSDdz),
                             boundary_conditions = (u=u_bcs, T=T_bcs)) 
 @show model
