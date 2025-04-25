9b525f4a3f7fdb4559dc4bdd4c5e819cfe9f7548
anpa5279 <anpa5279@colorado.edu>
2025-04-24 18:05:30 -0600

    latest comparison case tested against ncar les. changed sgs model

--- comparison.jl
+++ comparison.jl
@@ -1,132 +1,157 @@
-#attempting to match Smitth et al 2016 NCAR LES without angled stokes drift 
 using Pkg
 using MPI
+using CUDA
+using Statistics
+using Printf
 using Oceananigans
 using Oceananigans.DistributedComputations
-using Oceananigans.Units: minute, minutes, hours, hour
+using Oceananigans.Units: minute, minutes, hours, seconds
 using Oceananigans.BuoyancyFormulations: g_Earth
 
-#defining parameters
 mutable struct Params
-    Nx::Int
-    Ny::Int     # number of points in each of horizontal directions
-    Nz::Int     # number of points in the vertical direction
-    Lx::Float64
+    Nx::Int         # number of points in each of x direction
+    Ny::Int         # number of points in each of y direction
+    Nz::Int         # number of points in the vertical direction
+    Lx::Float64     # (m) domain horizontal extents
     Ly::Float64     # (m) domain horizontal extents
-    Lz::Float64     # (m) domain depth
+    Lz::Float64     # (m) domain depth 
+    N²::Float64     # s⁻², initial and bottom buoyancy gradient
+    initial_mixed_layer_depth::Float64 # m 
+    Q::Float64      # W m⁻², surface heat flux. cooling is positive
+    cᴾ::Float64     # J kg⁻¹ K⁻¹, specific heat capacity of seawater
+    ρₒ::Float64     # kg m⁻³, average density at the surface of the world ocean
+    dTdz::Float64   # K m⁻¹, temperature gradient
+    T0::Float64     # C, temperature at the surface   
+    β::Float64      # 1/K, thermal expansion coefficient
+    u₁₀::Float64    # (m s⁻¹) wind speed at 10 meters above the ocean
+    La_t::Float64   # Langmuir turbulence number
 end
 
-#defaults, these can be changed directly below
-params = Params(8, 8, 8, 128.0, 128.0,1.0)
+#defaults, these can be changed directly below 128, 128, 160, 320.0, 320.0, 96.0
+p = Params(128, 128, 160, 320.0, 320.0, 96.0, 5.3e-9, 33.0, 0.0, 4200.0, 1000.0, 0.01, 17.0, 2.0e-4, 5.75, 0.3)
 
-#global variables
-global u₁₀ = 6.0 # (m s⁻¹) wind speed at 10 meters above the ocean
+# Automatically distribute among available processors
+arch = Distributed(GPU())
+@show arch
+rank = arch.local_rank
+Nranks = MPI.Comm_size(arch.communicator)
+println("Hello from process $rank out of $Nranks")
 
-grid = RectilinearGrid(; size=(params.Nx, params.Ny, params.Nz), extent=(params.Lx, params.Ly, params.Lz))
-#@show grid
-# temperature and salinity boundary conditions
-buoyancy = SeawaterBuoyancy()
+grid = RectilinearGrid(arch; size=(p.Nx, p.Ny, p.Nz), extent=(p.Lx, p.Ly, p.Lz))
+@show grid
 
-Q = 1e-6 / hour # [°C s⁻¹]
-ρₒ = 1000.0 # [kg m⁻³]
-cᴾ = 3985.0 # [J kg⁻¹ K⁻¹]
-dTdz = -1e-3 # [°C m⁻¹]
-
-T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Q / (ρₒ * cᴾ)),
-                                bottom = GradientBoundaryCondition(dTdz))
-#@show T_bcs
-
-S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.0)) # no salt flux
-#@show S_bcs
-
-#functions
-function dstokes_dz(z)
+#stokes drift
+function stokes_velocity(z, u₁₀)
+    u = Array{Float64}(undef, length(z))
+    α = 0.00615
+    fₚ = 2π * 0.13 * g_Earth / u₁₀ # rad/s (0.22 1/s)
     a = 0.1
     b = 5000.0
-    Lf = b - a
     nf = 3^9
-    df = Lf / nf
-    σ = a + 0.5 * df
-    α = 0.00615
-    fₚ = 2π * 0.13 * g_Earth / u₁₀
-    for j in 1:length(z)
-        u = 0.0
-        for i in 1:nf
-            u = u + 2.0 * α * g_Earth / (fₚ * σ) * exp(2.0 * σ^2 * z[j] / g_Earth - (fₚ / σ)^4)
-            σ = σ + df
-        end 
-        u0 = df * u #stokes drift 
-        u = 0.0
+    df = (b -  a) / nf
+    for i in 1:length(z)
         σ = a + 0.5 * df
-        z1 = z[j] + 1e-6
-        for i in 1:nf
-            u = u + 2.0 * α * g_Earth / (fₚ * σ) * exp(2.0 * σ^2 * z1 / g_Earth - (fₚ / σ)^4)
+        u_temp = 0.0
+        for k in 1:nf
+            u_temp = u_temp + (2.0 * α * g_Earth / (fₚ * σ) * exp(2.0 * σ^2 * z[i] / g_Earth - (fₚ / σ)^4))
             σ = σ + df
         end 
-        u1 = df * u #stokes drift
-        dudz[j] = (u1 - u0) / (1e-6)
-        @show z[j]
-        @show u
-        @show dudz[j]
+        u[i] = df * u_temp
+    end
+    return u
+end
+function dstokes_dz(z, u₁₀)
+    dudz = Array{Float64}(undef, length(z))
+    α = 0.00615
+    fₚ = 2π * 0.13 * g_Earth / u₁₀ # rad/s (0.22 1/s)
+    a = 0.1
+    b = 5000.0
+    nf = 3^9
+    df = (b -  a) / nf
+    for i in 1:length(z)
+        σ = a + 0.5 * df
+        du_temp = 0.0
+        for k in 1:nf
+            du_temp = du_temp + (4.0 * α * σ/ (fₚ) * exp(2.0 * σ^2 * z[i] / g_Earth - (fₚ / σ)^4))
+            σ = σ + df
+        end 
+        dudz[i] = df * du_temp
     end
     return dudz
 end 
-@show dstokes_dz
-@show grid.Nz
-dudz = Array{Float64}(undef, grid.Nz)
-dudz = dstokes_dz(znodes(grid, Center()))
-@inline ∂z_uˢ(z, t) =
+const z_d = collect(reverse(-p.Lz + grid.z.Δᵃᵃᶜ/2 : grid.z.Δᵃᵃᶜ : -grid.z.Δᵃᵃᶜ/2))
+const dudz = dstokes_dz(z_d, p.u₁₀)
+new_dUSDdz = Field{Nothing, Nothing, Center}(grid)
+set!(new_dUSDdz, reshape(dudz, 1, 1, :))
 
-τx = 0.025 # N m⁻²
+u_f = p.La_t^2 * (stokes_velocity(-grid.z.Δᵃᵃᶜ/2, p.u₁₀)[1])
+τx = -(u_f^2)
 u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx))
-#@show u_bcs
+@show u_bcs
 
-#coriolis = FPlane(f=0.729e-4) # s⁻¹
+buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4), constant_salinity = 35.0)
+@show buoyancy
+T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(p.Q / (p.cᴾ * p.ρₒ * p.Lx * p.Ly)),
+                                bottom = GradientBoundaryCondition(p.dTdz))
+#coriolis = FPlane(f=1e-4) # s⁻¹
 
 model = NonhydrostaticModel(; grid, buoyancy, #coriolis,
                             advection = WENO(),
                             timestepper = :RungeKutta3,
-                            tracers = (:T, :S),
-                            closure = AnisotropicMinimumDissipation(),
-                            stokes_drift = UniformStokesDrift(∂z_uˢ=∂z_uˢ),
-                            boundary_conditions = (u=u_bcs, T=T_bcs, S=S_bcs))
-#@show model
+                            tracers = (:T),
+                            closure = SmagorinskyLilly(smagorinsky = 0.1),
+                            stokes_drift = UniformStokesDrift(∂z_uˢ=new_dUSDdz),
+                            boundary_conditions = (u=u_bcs, T=T_bcs)) 
+@show model
 
-# Temperature initial condition: a stable density gradient with random noise superposed.
-Tᵢ(x, y, z) = 20 + dTdz * z 
+# random seed
+Ξ(z) = randn() * exp(z / 4)
 
-u★ = sqrt(abs(τx))
-uᵢ(x, y, z) = u★ 
-wᵢ(x, y, z) = u★ 
+Tᵢ(x, y, z) = z > - p.initial_mixed_layer_depth ? p.T0 : p.T0 + p.dTdz * z + p.dTdz * model.grid.Lz * 1e-6 * Ξ(z)
+uᵢ(x, y, z) = u_f * 1e-1 * Ξ(z)
+wᵢ(x, y, z) = u_f * 1e-1 * Ξ(z)
 
-set!(model, u=uᵢ, w=wᵢ, T=Tᵢ, S=35)
+set!(model, u=uᵢ, w=wᵢ, T=Tᵢ)
 
-simulation = Simulation(model, Δt=45.0, stop_time = 4hours)
-#@show simulation
+simulation = Simulation(model, Δt=30.0, stop_time = 96hours)
+@show simulation
 
-conjure_time_step_wizard!(simulation, cfl=1.0, max_Δt=1minute)
+function progress(simulation)
+    u, v, w = simulation.model.velocities
 
-output_interval = 5minutes
+    # Print a progress message
+    msg = @sprintf("i: %04d, t: %s, Δt: %s, umax = (%.1e, %.1e, %.1e) ms⁻¹, wall time: %s\n",
+                   iteration(simulation),
+                   prettytime(time(simulation)),
+                   prettytime(simulation.Δt),
+                   maximum(abs, u), maximum(abs, v), maximum(abs, w),
+                   prettytime(simulation.run_wall_time))
+
+    @info msg
+
+    return nothing
+end
+
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(20))
+
+conjure_time_step_wizard!(simulation, cfl=0.5, max_Δt=30seconds)
+
+#output files
+function save_IC!(file, model)
+    file["IC/friction_velocity"] = u_f
+    file["IC/stokes_velocity"] = stokes_velocity(-grid.z.Δᵃᵃᶜ/2, p.u₁₀)[1]
+    file["IC/wind_speed"] = p.u₁₀
+    return nothing
+end
+
+output_interval = 10minutes
 
 fields_to_output = merge(model.velocities, model.tracers)
 
 simulation.output_writers[:fields] = JLD2OutputWriter(model, fields_to_output,
                                                       schedule = TimeInterval(output_interval),
-                                                      filename = "comparison_fields.jld2",
+                                                      filename = "langmuir_turbulence_fields_$rank.jld2",
                                                       overwrite_existing = true,
-                                                      with_halos = false)
-
-u, v, w = model.velocities
-
-U = Average(u, dims=(1, 2))
-V = Average(v, dims=(1, 2))
-wu = Average(w * u, dims=(1, 2))
-wv = Average(w * v, dims=(1, 2))
-
-simulation.output_writers[:averages] = JLD2OutputWriter(model, (; U, V, wu, wv),
-                                                        schedule = AveragedTimeInterval(output_interval, window=2minutes),
-                                                        filename = "comparison_averages.jld2",
-                                                        overwrite_existing = true,
-                                                        with_halos = false)
+                                                      init = save_IC!)
 
 run!(simulation)
--- stokes_profiles.jl
+++ stokes_profiles.jl
@@ -3,76 +3,83 @@ using Oceananigans
 using Oceananigans.Units: minute, minutes, hours, hour
 using Oceananigans.BuoyancyFormulations: g_Earth
 using CairoMakie
+using DelimitedFiles
 using Printf
 
 #functions
-function stokes_kernel(f, z, u10=6.0)
+#stokes drift
+function stokes_kernel(f, z, u₁₀)
     α = 0.00615
-    g = 9.81
-    fₚ = 2π * 0.13 * g / u10
-    return 2.0 * α * g / (fₚ * f) * exp(2.0 * f^2 * z / g - (fₚ / f)^4)
+    fₚ = 2π * 0.13 * g_Earth / u₁₀ # rad/s (0.22 1/s)
+    return 2.0 * α * g_Earth / (fₚ * f) * exp(2.0 * f^2 * z / g_Earth - (fₚ / f)^4)
 end
-
-function stokes_velocity(z)
+function stokes_velocity(z, u₁₀)
+    u = Array{Float64}(undef, length(z))
     a = 0.1
     b = 5000.0
-    Lf = b - a
     nf = 3^9
-    df = Lf / nf
-    σ = a + 0.5 * df
-    u = 0.0
-    for _ in 1:nf
-        u = u + stokes_kernel(σ, z)
-        σ = σ + df
-    end 
-    return df * u
+    df = (b -  a) / nf
+    for i in 1:length(z)
+        σ1 = a + 0.5 * df
+        u_temp = 0.0
+        for k in 1:nf
+            u_temp = u_temp + stokes_kernel(σ1, z[i], u₁₀)
+            σ1 = σ1 + df
+        end 
+        u[i] = df * u_temp
+    end
+    return u
 end
-
-function dstokes_dz(z, t)
-    u0 = stokes_velocity(z)
-    u1 = stokes_velocity(z + 1e-6)
-    dudz = (u1 - u0) / (1e-6)
+function dstokes_dz(z, u₁₀)
+    dudz = Array{Float64}(undef, length(z))
+    α = 0.00615
+    fₚ = 2π * 0.13 * g_Earth / u₁₀ # rad/s (0.22 1/s)
+    a = 0.1
+    b = 5000.0
+    nf = 3^9
+    df = (b -  a) / nf
+    for i in 1:length(z)
+        σ = a + 0.5 * df
+        du_temp = 0.0
+        for k in 1:nf
+            du_temp = du_temp + (4.0 * α * σ/ (fₚ) * exp(2.0 * σ^2 * z[i] / g_Earth - (fₚ / σ)^4))
+            σ = σ + df
+        end 
+        dudz[i] = df * du_temp
+    end
     return dudz
 end 
-
-function plot()
-    #grid setup
-    grid = RectilinearGrid(size = 8, z = (-1.0, 0.0), topology=(Flat, Flat, Bounded))
-    Nz = grid.Nz
-    Lz = grid.Lz
-    z = grid.z.cᵃᵃᶜ[1:Nz]
-    u_data = Array{Float64}(undef, (1, 1, grid.Nz))
-    dudz_data = Array{Float64}(undef, (1, 1, grid.Nz))
-
-    #calculating stokes drift in z-direction
-    for k in 1:Nz
-        z_pt = z[k]
-        u_data[k] = stokes_velocity(z_pt)
-        dudz_data[k] = dstokes_dz(z_pt, 0.0)
-        @show z_pt
-        @show u_data[k]
-        @show dudz_data[k]
-    end
-
-    u = FieldTimeSeries{Face, Center, Center}(grid, 0.0)
-    dudz = FieldTimeSeries{Face, Center, Center}(grid, 0.0)
-
-    u .= u_data
-    dudz .= dudz_data
-
-    #plotting velocity profile
-    n = Observable(1)
-    fig = Figure()
-    ax = Axis(fig[1, 1], xlabel = "uˢ [m/s]", ylabel = "z [m]", title = "Stokes drift")
-    Uₙ = @lift view(u[$n], 1, 1, :)
-    lines!(ax, Uₙ)
-    fig 
-    save("stokes_drift.png", fig)
-    #plotting velocity gradient profile
-    fig2 = Figure()
-    ax2 = Axis(fig2[1, 1], xlabel = "duˢ/dz [1/s]", ylabel = "z [m]", title = "Stokes drift gradient")
-    dUₙ = @lift view(dudz[$n], 1, 1, :)
-    lines!(ax2, dUₙ)
-    fig2
-    save("stokes_drift_gradient.png", fig2)
-end 
+#calculating stokes drift in z-direction
+z = collect(znodes(grid, Center()))
+u_data = Array{Float64}(undef, (Nz))
+dudz_data = Array{Float64}(undef, (Nz))
+for k in 1:Nz
+    z_pt = z[k]
+    u_data[k] = stokes_velocity(z_pt, 5.75)[1]
+    dudz_data[k] = dstokes_dz(z_pt, 5.75)[1]
+end
+#read in ncar les profiles
+ncarles = readdlm("NCAR-LES-stokes.txt", ' ')
+ncarles_stokes = reverse(ncarles[:, 3])
+ncarles_z = reverse(ncarles[:, 2])
+ncarles_dudz = Array{Float64}(undef, length(ncarles_z)-1)
+for i in 1:length(ncarles_z) - 1
+    ncarles_dudz[i] = (ncarles_stokes[i] - ncarles_stokes[i + 1])/(ncarles_z[i] - ncarles_z[i + 1])
+end
+#plotting velocity profile
+n = Observable(1)
+fig = Figure()
+ax = Axis(fig[1, 1], xlabel = "uˢ [m/s]", ylabel = "z [m]", title = "Stokes drift")
+lines!(ax, u_data, z, label = "Oceananigans Stokes drift")
+lines!(ax, ncarles_stokes, ncarles_z, label = "NCAR-LES Stokes drift")
+axislegend(ax; position = :rb)
+fig 
+save("stokes_drift.png", fig)
+#plotting velocity gradient profile
+fig2 = Figure()
+ax2 = Axis(fig2[1, 1], xlabel = "duˢ/dz [1/s]", ylabel = "z [m]", title = "Stokes drift gradient")
+lines!(ax2, dudz_data, z, label = "Oceananigans Stokes drift gradient")
+lines!(ax2, ncarles_dudz, ncarles_z[2:length(ncarles_z)], label = "NCAR-LES Stokes drift gradient")
+fig2
+axislegend(ax2; position = :rb)
+save("stokes_drift_gradient.png", fig2)
